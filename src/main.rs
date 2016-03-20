00a16c960b98a18cc8db0b36614ab246f0e55fd8
Geoff Cant <nem@erlang.geek.nz>
2016-03-19 18:47:03 -0700

    Change listen address if running locally.
    Otherwise OS X prompts you for firewall approval.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate pencil;
 use pencil::{Pencil, Request, Response, PencilResult};
 use std::env;
 use std::str::FromStr;
+use std::path::PathBuf;
 
 fn hello(_: &mut Request) -> PencilResult {
     Ok(Response::from("Hello World!"))
@@ -13,11 +14,20 @@ fn listen_port() -> u16 {
     FromStr::from_str(&port_str).unwrap_or(5000)
 }
 
+fn static_path() -> PathBuf {
+    env::current_dir().unwrap_or(env::home_dir().unwrap_or(env::temp_dir()))
+}
+
+fn running_on_heroku() -> bool {
+    env::var("DYNO").is_ok()
+}
+
 fn main() {
+    let listen_addr = if running_on_heroku() { "0.0.0.0" } else { "127.0.0.1" };
     let port = listen_port();
-    let listen_addr = format!("0.0.0.0:{}", port);
-    println!("Listening at {}.", listen_addr);
-    let mut app = Pencil::new("/web/hello");
+    let root_path = static_path().to_string_lossy().into_owned();
+    println!("Listening at {}:{} (root: {}).", listen_addr, port, root_path);
+    let mut app = Pencil::new(&root_path);
     app.get("/", "hello", hello);
-    app.run(("0.0.0.0", port));
+    app.run((listen_addr.as_ref(), port));
 }
